90de087023908a09361adba3c4bb1b5243558924
David Moya <3nueves@gmail.com>
2024-07-01 08:24:37 +0200

    update

--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@
 docker run --name mariadb-backup --rm \
 -v /var/log/:/var/log \
 -e DB_EXCLUDE="mysql performance_schema information_schema" \
--e DB_PASSWORD=wzkehrAjyIP3hsaH \
+-e DB_PASSWORD=password \
 -e DB_USER=root \
--e DB_HOST="10.40.0.212" \
--e MINIO_HOST="10.0.0.83:9000" \
+-e DB_HOST="10.50.0.212" \
+-e MINIO_HOST="10.20.0.83:9000" \
 -e MINIO_ACCESS_KEY=minio \
--e MINIO_SECRET_KEY=yVyI54Lh9QgH \
+-e MINIO_SECRET_KEY=password \
 -e MINIO_BUCKET_NAME=backup-all-dbs \
 -it mariadb-backup:v2.0.0
 # -it registry.gitlab.com/invisible_bits/docker/mysql-backups:latest
